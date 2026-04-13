7b26281d94180d02b7976e24b8f8a792ca36e434
J3vrin96 <jeremy.quevrin@hotmail.com>
2026-04-13 15:19:32 +0200

    chore: fix eslint config for css files

--- eslint.config.ts
+++ eslint.config.ts
@@ -6,32 +6,35 @@ import css from "@eslint/css";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-  {
-    ignores: [
-      ".nuxt/**",
-      "dist/**",
-      ".output/**",
-      "node_modules/**",
-      "package-lock.json",
-    ],
-  },
-  {
-    files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
-    plugins: { js },
-    extends: ["js/recommended"],
-    languageOptions: { globals: globals.browser },
-  },
-  tseslint.configs.recommended,
-  {
-    files: ["**/*.json"],
-    plugins: { json },
-    language: "json/json",
-    extends: ["json/recommended"],
-  },
-  {
-    files: ["**/*.css"],
-    plugins: { css },
-    language: "css/css",
-    extends: ["css/recommended"],
-  },
+	{
+		ignores: [
+			'.nuxt/**',
+			'dist/**',
+			'.output/**',
+			'node_modules/**',
+			'package-lock.json',
+		],
+	},
+	{
+		files: ['**/*.{js,mjs,cjs,ts,mts,cts}'],
+		plugins: { js },
+		extends: ['js/recommended'],
+		languageOptions: { globals: globals.browser },
+	},
+	tseslint.configs.recommended,
+	{
+		files: ['**/*.json'],
+		plugins: { json },
+		language: 'json/json',
+		extends: ['json/recommended'],
+	},
+	{
+		files: ['**/*.css'],
+		plugins: { css },
+		language: 'css/css',
+		extends: ['css/recommended'],
+		rules: {
+			'css/no-invalid-properties': 'off',
+		},
+	},
 ]);
